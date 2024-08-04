e46ff27bb682c6b44f59695bbc9a8e8bfd4440b9
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-04 16:47:52 +0900

    🎸 Chore: 프로젝트 파일 업데이트

--- RxSwiftBasic/SceneDelegate.swift
+++ RxSwiftBasic/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+//        let vc = UINavigationController(rootViewController: ViewController())
+        let vc = UINavigationController(rootViewController: LoginViewController())
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
 
