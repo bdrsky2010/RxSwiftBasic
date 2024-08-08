317f531f7ce32cba7ea176d826b8c85231324296
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-08 17:30:22 +0900

    💾 Add: 빈 화면 구성

---
+++ RxSwiftBasic/iTunesAPISearch/EmptyViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EmptyViewController.swift
+//  RxSwiftBasic
+//
+//  Created by Minjae Kim on 8/8/24.
+//
+
+import UIKit
+
+final class EmptyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
