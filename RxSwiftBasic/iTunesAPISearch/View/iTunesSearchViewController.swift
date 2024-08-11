afc0e148fa8043bc3fe5bee329e7d57f5d249ea8
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-11 22:51:55 +0900

    💾 Add: CompositionalLayout을 활용하여 검색결과를 보여주는 컬렉션뷰 구성

--- RxSwiftBasic/iTunesAPISearch/View/iTunesSearchViewController.swift
+++ RxSwiftBasic/iTunesAPISearch/View/iTunesSearchViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import RxCocoa
+import RxDataSources
 import RxSwift
 import SnapKit
 
@@ -15,26 +16,94 @@ final class iTunesSearchViewController: UIViewController {
     private let searchController: UISearchController = {
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "게임, 앱, 스토리 등"
+        searchController.searchBar.setValue("취소", forKey: "cancelButtonText")
         return searchController
     }()
     
+    private let resultCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    
+    private let viewModel = iTunesSearchViewModel()
+    private let cellViewModel = iTunesSearchCellViewModel()
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         navigationItem.searchController = searchController
+        configureView()
+        bind()
+    }
+    
+    private func bind() {
+        let cancelButtonTap = PublishSubject<Void>()
+        let searchTextChange = PublishSubject<String>()
+        let searchButtonTap = PublishSubject<String>()
         
-        let url = "https://itunes.apple.com/search?term=kakaotalk&entity=software&country=kr&lang=ko_kr&limit=10"
-        NetworkManager.shared.requestAPIWithSingle(url: url, of: iTunesSearch.self)
-            .subscribe(with: self) { owner, value in
-                dump(value)
-            } onFailure: { owner, error in
-                print("error: \(error)")
-            } onDisposed: { owner in
-                print("disposed")
-            }
+        searchController.searchBar.rx.cancelButtonClicked
+            .bind(to: cancelButtonTap)
             .disposed(by: disposeBag)
+        
+        searchController.searchBar.rx.text.orEmpty
+            .bind(to: searchTextChange)
+            .disposed(by: disposeBag)
+        
+        searchController.searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchController.searchBar.rx.text.orEmpty)
+            .bind(to: searchButtonTap)
+            .disposed(by: disposeBag)
+        
+        let input = iTunesSearchViewModel.Input(cancelButtonTap: cancelButtonTap,
+                                                searchTextChange: searchTextChange,
+                                                searchButtonTap: searchButtonTap)
+        
+        let output = viewModel.transform(input: input)
+        
+        let dataSource = RxCollectionViewSectionedReloadDataSource<SectioniTunesSearch> { [weak self] dataSource, collectionView, indexPath, item in
+            guard let self, let cell = collectionView.dequeueReusableCell(withReuseIdentifier: iTunesSearchCollectionViewCell.identifier, for: indexPath) as? iTunesSearchCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.configureContent(searchApp: item)
+            
+            let trackViewUrl = Observable.just(item.trackViewUrl)
+            let downloadButtonTap = cell.downloadButton.rx.tap
+                .withLatestFrom(trackViewUrl)
+            let input = iTunesSearchCellViewModel.Input(disposeBag: cell.disposeBag, downloadButtonTap: downloadButtonTap)
+            cellViewModel.transform(input: input)
+            return cell
+        }
+        
+        output.requestResult
+            .bind(to: resultCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureView() {
+        view.addSubview(resultCollectionView)
+        
+        resultCollectionView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        resultCollectionView.register(iTunesSearchCollectionViewCell.self,
+                                      forCellWithReuseIdentifier: iTunesSearchCollectionViewCell.identifier)
+    }
+}
+
+extension iTunesSearchViewController {
+    static func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 0, bottom: 30, trailing: 0)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.6))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
