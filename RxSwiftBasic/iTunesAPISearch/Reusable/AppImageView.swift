c2759551786c363fc0e4c5decd76d09150e0ed6a
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-11 22:48:27 +0900

    💾 Add: API 검색 결과를 보여줄 때 이미지뷰로 사용될 재사용 커스텀이미지뷰 구성

---
+++ RxSwiftBasic/iTunesAPISearch/Reusable/AppImageView.swift
@@ -0,0 +1,30 @@
+//
+//  AppImageView.swift
+//  RxSwiftBasic
+//
+//  Created by Minjae Kim on 8/11/24.
+//
+
+import UIKit
+
+import Kingfisher
+import SnapKit
+
+final class AppImageView: UIImageView {
+    
+    init(cornerRadius: CGFloat) {
+        super.init(frame: .zero)
+        contentMode = .scaleAspectFit
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+    }
+    
+    func setImage(imageUrl: String) {
+        kf.setImage(with: URL(string: imageUrl), options: [.transition(.fade(1))])
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
