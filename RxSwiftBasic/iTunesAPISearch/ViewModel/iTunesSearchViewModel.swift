8a344c7be8e94072a1dbe4d73c090f3cece9ff80
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-13 00:02:32 +0900

    🩹 Refactor & 🧑🏻‍💻 Feat & 👾 Fix: Result로 래핑된 Single 타입을 전달받아 subscribe를 통해 데이터를 바인드
    - 그냥 URLSession에서 네트워크 통신하여 에러를 전달하고 뷰에서 토스트 메시지를 띄워주려 했음
    - 근데 백그라운드 스레드에서 돌아가고 있으니 확인해보라며 앱을 크래시 해버림
    - 에러를 일부러 발생시켜 메인스레드인지 확인해보니 백그라운드 스레드에서 돌고 있었음
    - 뷰컨트롤러에서 subscribe가 아닌 bind로 데이터를 바인딩해서 그런가 해서 테스트를 해보니 그건 아니었음
    - 그래서 애초에 네트워크 통신할 때 메인스레드를 보장해주도록 asDriver() 메서드를 호출하여 드라이버로 래핑하였음
    - 근데 문제가 하나 있음. 네트워크 통신 결과로 빈 배열이 들어오면 부모스트림이 dispose되어버림... 이거는 좀 찾아서 해결해봐야됨

--- RxSwiftBasic/iTunesAPISearch/ViewModel/iTunesSearchViewModel.swift
+++ RxSwiftBasic/iTunesAPISearch/ViewModel/iTunesSearchViewModel.swift
@@ -19,10 +19,12 @@ final class iTunesSearchViewModel: BaseViewModel {
     
     struct Output {
         let requestResult: PublishSubject<[SectioniTunesSearch]>
+        let requestError: PublishSubject<String>
         let SearchDetailPush: PublishSubject<SearchApp>
     }
     
     private let searchResult = PublishSubject<[SectioniTunesSearch]>()
+    private let searchError = PublishSubject<String>()
     private let disposeBag = DisposeBag()
     
     private let firstLimit = 10
@@ -53,23 +55,34 @@ final class iTunesSearchViewModel: BaseViewModel {
                 
                 let url = owner.getiTunesAPIUrl(query: owner.requestQuery, limit: owner.requestLimit)
                 NetworkManager.shared.requestAPIWithSingle(url: url, of: iTunesSearch.self)
-                    .subscribe(with: self) { owner, value in
-                        print("result count: \(value.resultCount)")
-                        owner.searchResult.onNext([SectioniTunesSearch(items: value.results)])
-                    } onFailure: { owner, error in
-                        print("error: \(error)")
-                    } onDisposed: { owner in
+                    .asDriver(onErrorJustReturn: .failure(.unknown))
+                    .debug("asDriver")
+                    .drive(with: self) { owner, result in
+                        switch result {
+                        case .success(let value):
+                            print("result count: \(value.resultCount)")
+                            owner.searchResult.onNext([SectioniTunesSearch(items: value.results)])
+                        case .failure(let error):
+                            print(error.message)
+                            owner.searchError.onNext(error.message)
+                        }
+                    } onCompleted: { _ in
+                        print("completed")
+                    } onDisposed: { _ in
                         print("disposed")
                     }
                     .disposed(by: owner.disposeBag)
             }
             .disposed(by: disposeBag)
         
-        return Output(requestResult: searchResult, SearchDetailPush: input.cellSelect)
+        return Output(requestResult: searchResult,
+                      requestError: searchError,
+                      SearchDetailPush: input.cellSelect)
     }
     
     private func getiTunesAPIUrl(query: String, limit: Int) -> String {
         let urlString = "https://itunes.apple.com/search?term=\(query)&entity=software&country=kr&lang=ko_kr&limit=\(limit)"
+//        let urlString = "https://itunes.apple.com/search?term=&entity=software&country=kr&lang=ko_kr&limit=\(limit)"
         return urlString
     }
 }
